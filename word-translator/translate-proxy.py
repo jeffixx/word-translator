669940b745bcf439589c1ecd9e2310944796f969
jeffixx <51029007+jeffixx@users.noreply.github.com>
2026-06-15 01:00:15 +0800

    add html

--- word-translator/translate-proxy.py
+++ word-translator/translate-proxy.py
@@ -2,13 +2,19 @@ import http.server
 import urllib.request
 import json
 import sys
-
 import os
+
 PORT = int(os.environ.get("PORT", 8080))
 
 class ProxyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path.startswith('/freedict/'):
+        if self.path == '/' or self.path == '/index.html':
+            self._serve_static('index.html', 'text/html; charset=utf-8')
+        elif self.path.startswith('/css/'):
+            self._serve_static(self.path[1:], 'text/css; charset=utf-8')
+        elif self.path.startswith('/js/'):
+            self._serve_static(self.path[1:], 'application/javascript; charset=utf-8')
+        elif self.path.startswith('/freedict/'):
             word = self.path[len('/freedict/'):]
             url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + urllib.request.quote(word)
             self._proxy(url)
@@ -17,6 +23,18 @@ class ProxyHandler(http.server.BaseHTTPRequestHandler):
             url = 'https://api.mymemory.translated.net/get?q=' + urllib.request.quote(word) + '&langpair=en|zh-CN'
             self._proxy(url)
         else:
+            self.send_error(404)
+
+    def _serve_static(self, filename, content_type):
+        try:
+            with open(filename, 'rb') as f:
+                data = f.read()
+            self.send_response(200)
+            self.send_header('Content-Type', content_type)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            self.wfile.write(data)
+        except FileNotFoundError:
             self.send_error(404)
 
     def _proxy(self, url):
@@ -52,7 +70,7 @@ class ProxyHandler(http.server.BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     server = http.server.HTTPServer(('0.0.0.0', PORT), ProxyHandler)
-    print('翻译代理已启动: http://127.0.0.1:' + str(PORT))
+    print('翻译代理已启动: http://0.0.0.0:' + str(PORT))
     print('  /freedict/<word>  - Free Dictionary API')
     print('  /mymemory/<word>  - MyMemory翻译API')
     try:
